463a75b9cedb9aedf9259bfeca38c354f1d6107d
jo-es <45110941+jo-es@users.noreply.github.com>
2021-08-04 15:02:56 +0200

    Fix network config

--- network.ts
+++ network.ts
@@ -25,7 +25,7 @@ export const NETWORK_ENV = {
     'UniswapV3Router': '0xE592427A0AEce92De3Edee1F18E0157C05861564', // Uniswap V3 Router
     'UniswapV3Quoter': '0xb27308f9F90D607463bb33eA1BeBb41C27CE5AB6', // Uniswap V3 Quoter
     'WETH': '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-    'DAI': '',
+    'DAI': '0x6B175474E89094C44Da98b954EedeAC495271d0F',
     'USDC': '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
     'WBTC': '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599'
   }
